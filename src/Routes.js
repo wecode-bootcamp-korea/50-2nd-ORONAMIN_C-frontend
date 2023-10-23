18b1c1bf005fca30a7589e01d2e67b04cadf1fdb
jsm00929@gmail.com <jsm00929@naver.com>
2023-10-23 17:41:52 +0900

    초기 세팅

---
+++ src/Routes.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+const Routes = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default Routes;
