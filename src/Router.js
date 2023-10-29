2270aef257f362e2dc2908c7d781900a6ea60b82
Hyun Yong Jeong <jhy3579012@gmail.com>
2023-10-29 11:11:33 +0900

    메인 헤더 합병

--- src/Router.js
+++ src/Router.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Header from './components/Nav/Layers';
+import Nav from './components/Nav/Nav';
 import Main from './pages/main/Main';
 
 const Router = () => {
   return (
     <BrowserRouter>
+      <Nav />
       <Routes>
-        <Route path="/" element={<Header />} />
         <Route path="/main" element={<Main />} />
       </Routes>
     </BrowserRouter>
