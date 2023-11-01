c5272e4ea0ed0071bb7690a2482d7d984eeed4c1
Hsoohyeon <soohyeon062800@gmail.com>
2023-11-01 20:00:04 +0900

    디테일페이지 장바구니버튼구현완료

--- src/pages/ProductDetail/ProductDetail.js
+++ src/pages/ProductDetail/ProductDetail.js
@@ -1,16 +1,19 @@
 import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import './ProductDetail.scss';
 
 const ProductDetail = () => {
   const [productInfo, setProductInfo] = useState([]);
-  const { id } = useParams();
-  // console.log(id);
+  const navigate = useNavigate();
+  const { productId } = useParams();
+
+  const token =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NSwiZW1haWwiOiJrYWthb2RkZGRkQG5hdmVyLmNvbSIsIm5pY2tuYW1lIjoi7J287JqU7J28IOuwpOydmCDsi6zsi6ztlZwg6rOg7Iq064-E7LmYIiwic3RhdHVzIjowLCJpYXQiOjE2OTgzOTA2MzcsImV4cCI6MTczNDM5MDYzN30.FzeJQLsft8Z8nWsleGXGwWuqLsB6u-HzLNA-PsZ0pCA';
 
   // `http://10.58.52.234:8000/product-list/detail/${productId}`
   // '/data/recommendData.json'
   useEffect(() => {
-    fetch('/data/productDetail.json', {
+    fetch(`http://10.58.52.234:8000/products/${productId}`, {
       method: 'GET',
       headers: {
         'Content-type': 'application/json',
@@ -18,8 +21,25 @@ const ProductDetail = () => {
     })
       .then(response => response.json())
       .then(result => setProductInfo(result.product));
-  }, [id]);
-  // console.log(setProductInfo());
+  }, [productId]);
+
+  const goToAddProduct = ({ productId }) => {
+    fetch('http://10.58.52.220:8000/users/addBusket', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json;charset=utf-8',
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify({
+        product_id: productId,
+      }),
+    }).then(res => {
+      if (res.ok) {
+        alert('장바구니 담기 완료!');
+        navigate(`/product-list/detail/${productId}`);
+      }
+    });
+  };
 
   const formatPriceWithCommas = price => {
     return price.toLocaleString();
@@ -40,7 +60,12 @@ const ProductDetail = () => {
                 ₩{formatPriceWithCommas(data.price)}
               </h4>
             </div>
-            <button className="btn">카트에 추가담기</button>
+            <button
+              className="btn"
+              onClick={() => goToAddProduct({ productId: data.id })}
+            >
+              카트에 추가담기
+            </button>
             <div className="itemDetailInfo3">
               <label className="itemScent">향</label>
               <hr />
